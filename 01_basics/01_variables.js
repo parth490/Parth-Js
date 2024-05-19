8220bbf59c5c9c55a66bb5b9c2d03ff57a9293d1
Patel ParthKumar <133362510+parth490@users.noreply.github.com>
2024-05-19 16:27:06 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12222
+let accountEmail = "parth007@gmail.com"
+var accountPassword = "1234"
+accountCity = "surat"
+
+
+// accountId ="2" //
+accountEmail ="hfg@gmail.com"
+accountPassword = "2221212"
+accountCity ="Gandhinagar"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity]);
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// allert("hello") we r using node js, not browser
+
+console.log("age");
+
+let name = "hitesh"
+let age = 18
+
+console.log(typeof age);
